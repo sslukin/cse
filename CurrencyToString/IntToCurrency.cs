29ae035aafba417389c9308ea108c1c01dd147a5
Sergey Slukin <sslukin@gmail.com>
2021-02-24 20:23:27 +0300

    1st working version

---
+++ CurrencyToString/IntToCurrency.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyToString
+{
+    static class IntToCurrency
+    {
+        public static string Convert(this int n)
+        {
+            List<string> result = new List<string>();
+
+            int d1, d2, d3, d23;
+            d1 = (n - n % 100) / 100;
+            d23 = n - (d1 * 100);
+            d3 = d23 % 10;
+            d2 = (d23 - d3) / 10;
+
+            string[] arrD1 = ",сто,двести,триста,четыреста,пятьсот,шестьсот,семьсот,восемьсот,девятьсот".Split(",");
+            if (d1 > 0) result.Add(arrD1[d1]);
+
+            string[] arrD2 = ",десять,двадцать,тридцать,сорок,пятьдесят,шестьдесят,семьдесят,восемьдесят,девяносто".Split(",");
+            if (d23 > 10 && d23 < 20)
+            {
+                string[] arrD23 = ",одиннадцать,двенадцать,тринадцать,четырнадцать,пятнадцать,шестнадцать,семнадцать,восемнадцать,девятнадцать".Split(",");
+                result.Add(arrD23[d3]);
+            }
+            else
+            {
+                if (d2 > 0) result.Add(arrD2[d2]);
+
+                string[] arrD3 = ",один,два,три,четыре,пять,шесть,семь,восемь,девять".Split(",");
+                if (d3 > 0) result.Add(arrD3[d3]);
+            }
+
+            string rub;
+            if (d3 == 1 && d2 != 1)
+                rub = "рубль";
+            else if ((d3 == 2 || d3 == 3 || d3 == 4) && d2 != 1)
+                rub = "рубля";
+            else
+                rub = "рублей";
+
+            result.Add(rub);
+
+            return String.Join(" ", result);
+        }
+    }
+}
---
+++ CurrencyToString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace CurrencyToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Enter a number");
+            string number = Console.ReadLine();
+  
+            int n;
+
+            if (int.TryParse(number, out n))
+            {
+                Console.WriteLine(n.Convert());
+            }
+            else
+                Console.WriteLine("Not a number!");
+        }
+    }
+}
